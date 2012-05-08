4e1531efd8c2732e8eb97b1b0d57bcbf6184594d
FYANES <FYANES@FYANES>
2012-05-07 21:44:18 -0400

    Conexion realizada

--- JAlmacen/src/datos/Conexion.java
+++ JAlmacen/src/datos/Conexion.java
@@ -4,10 +4,48 @@
  */
 package datos;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author FYANES
  */
 public class Conexion {
+    private String dsn = "jdbc:mysql://localhost:3306/jalmacen";
+    private String user = "root";
+    private String pass= "";
+    private Connection cnx;
+    public Statement st;
+    private Conexion() {
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            cnx= DriverManager.getConnection(dsn,user,pass);
+            st = cnx.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "error conexion");
+        }
+    }
     
+    public static Conexion getInstance() {
+        return ConexionHolder.INSTANCE;
+    }
+    
+    private static class ConexionHolder {
+
+        private static final Conexion INSTANCE = new Conexion();
+    }
+    public static void main(String args[]) {
+        Conexion c = new Conexion();
+        try {
+            ResultSet r = c.st.executeQuery("select * from empresas");
+            r.next();
+            JOptionPane.showMessageDialog(null, r.getString("EMP_RAZON"));
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	System.out.println("Un despliegue de Datos");
+    }
 }
