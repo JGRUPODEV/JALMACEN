6d47da2a2d1f761e594f7b6d37ea3befc346bc36
FYANES <FYANES@FYANES>
2012-05-09 16:56:03 -0400

    Conexion realizada

--- JAlmacen/src/datos/Conexion.java
+++ JAlmacen/src/datos/Conexion.java
@@ -12,38 +12,41 @@ import javax.swing.JOptionPane;
  * @author FYANES
  */
 public class Conexion {
+
     private String dsn = "jdbc:mysql://localhost:3306/jalmacen";
     private String user = "root";
-    private String pass= "";
-    private Connection cnx;
-    public Statement st;
-    private Conexion() {
-        try{
+    private String pass = "";
+    private Connection cnx = null;
+    private Connection crear() {
+        try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
-            cnx= DriverManager.getConnection(dsn,user,pass);
-            st = cnx.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
-        }catch(Exception e){
+            cnx = DriverManager.getConnection(dsn, user, pass);  
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "error conexion");
+            return null;
         }
+        return cnx;
     }
-    
-    public static Conexion getInstance() {
-        return ConexionHolder.INSTANCE;
-    }
-    
-    private static class ConexionHolder {
 
-        private static final Conexion INSTANCE = new Conexion();
+    public Connection getInstancia() {
+        if (cnx == null) {
+            return crear();
+        }
+        return cnx;
     }
+
     public static void main(String args[]) {
         Conexion c = new Conexion();
+        Connection cn = c.getInstancia();
         try {
-            ResultSet r = c.st.executeQuery("select * from empresas");
+            Statement st;
+            st = cn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet r = st.executeQuery("select * from empresas");
             r.next();
             JOptionPane.showMessageDialog(null, r.getString("EMP_RAZON"));
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null,"error consulta");
         }
-	System.out.println("Un despliegue de Datos");
+        System.out.println("Un despliegue de Datos");
     }
 }
